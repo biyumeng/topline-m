e8755fa84d6ce2d2525e502609db9d5014a50d82
biyumeng <609814862@qq.com>
2020-01-11 11:41:54 +0800

    首页-使用Vant组件快速搭建页面结构

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -14,7 +14,11 @@ import { Button,
   Image,
   Grid,
   GridItem,
-  Dialog } from 'vant'
+  Dialog,
+  Tab,
+  Tabs,
+  List,
+  PullRefresh } from 'vant'
 
 // 注册vant组件
 Vue.use(Button)
@@ -31,3 +35,7 @@ Vue.use(Button)
   .use(Grid)
   .use(GridItem)
   .use(Dialog)
+  .use(Tab)
+  .use(Tabs)
+  .use(List)
+  .use(PullRefresh)
