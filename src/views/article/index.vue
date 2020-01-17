cdb9bd17663deb157a86f80b774e080efe72313c
biyumeng <609814862@qq.com>
2020-01-17 11:44:32 +0800

    文章详情-创建组件并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="article-container">文章详情页</div>
+</template>
+
+<script>
+export default {
+  name: 'ArticlePage',
+  components: {},
+  props: {
+    // 路由参数
+    articleId: {
+      type: [String, Object, Number],
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
